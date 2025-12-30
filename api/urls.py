9912cd36888d6a918c7497c427e77044f997e248
abdurauf0788 <turkeynumber063@gmail.com>
2025-12-30 10:34:53 +0500

    tayyor

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from .views import ProductViewSet
 
 urlpatterns = [
     # path("products/", ProductListCreateView.as_view(), name="products" ),
-    path("products/<int:pk>/", ProductViewSet.as_view({'get':'retieve', 'put':'update', 'delete':'destroy'})),
+    path("products/<int:pk>/", ProductViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'})),
     path("products/", ProductViewSet.as_view({'get':'list', 'post':'create'})),
 
 ]
